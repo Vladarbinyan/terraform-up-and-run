2188e7135f75851c05d51dc18c219a3863273971
clab <clab@cnt7.home>
2021-07-24 22:49:09 +0700

    add "Hello World" web server

--- aws/main.tf
+++ aws/main.tf
@@ -14,7 +14,27 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-    tags = {
+  # <PROVIDER>_<TYPE>.<NAME>.<ATTRIBUTE>
+  vpc_security_group_ids = [aws_security_group.allow_8080.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
     Name = "CheapWorker"
+  }
+}
+
+resource "aws_security_group" "allow_8080" {
+  name        = "allow_8080"
+  description = "Allow inbound traffic to port 8080"
+
+  ingress {
+    description = "custom http port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
